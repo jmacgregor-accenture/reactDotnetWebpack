25f1c0971c130ee4e1ec11ddb213d6f8ccf1df9a
John MacGregor <jonathan.macgregor@accenture.com>
2019-04-08 15:12:37 -0400

    Using webpack.config

---
+++ ReactWebpackTrial/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+    entry: './wwwroot/src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'wwwroot/dist'),
+        filename: 'bundle.js'
+    },
+    mode: 'development'
+};
